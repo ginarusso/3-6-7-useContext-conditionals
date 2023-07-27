cb5357f657d5bdd62250890b9e340704d64e99e4
Gina Russo <gpr0496@gmail.com>
2023-07-27 12:17:24 -0400

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Header from "./Header"
+import Button from "./Button"
+import './App.css'
+
+const ThemeContext = React.createContext()
+
+export default function App() {
+    const [theme, setTheme] = React.useState("dark")
+
+    function toggleTheme() {
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+    }
+    
+    return (
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            <div className={`container ${theme}-theme`}>
+                <Header />
+                <Button />
+            </div>
+        </ThemeContext.Provider>
+    )
+}
+
+export { ThemeContext }
